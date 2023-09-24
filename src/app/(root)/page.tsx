040e8b3b12d2d8e2b1abd339b3eecd254984611e
Serhii Korniichuk <serhii.korniichuk.dev@gmail.com>
2023-09-24 16:45:13 +0300

    refactor: prepare Home page

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,10 +1,5 @@
-import Image from 'next/image'
 import styles from './page.module.scss'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <Image src="/next.svg" alt="logo" width={48} height={48} />
-    </main>
-  )
+  return <main className={styles.main}>{/*<Page />*/}</main>
 }
